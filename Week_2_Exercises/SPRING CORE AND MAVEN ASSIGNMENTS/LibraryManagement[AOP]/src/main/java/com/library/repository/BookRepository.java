c634d2dbf6bfb3b41c897e46d2fd74e9e4943e4d
Sivaganesh Natarajavel <115616078+Sivag1203@users.noreply.github.com>
2024-08-08 10:13:14 +0530

    Add files via upload

---
+++ Week_2_Exercises/SPRING CORE AND MAVEN ASSIGNMENTS/LibraryManagement[AOP]/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.library.repository;
+
+public class BookRepository {
+    public  BookRepository(){
+        System.out.println("Book repository class object is created using spring");
+    }
+    public  void success(){
+        System.out.println("Vanthuruchu da mapla");
+    }
+}
